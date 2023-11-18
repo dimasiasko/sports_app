a77a7fcc5264776239c32962ebe0fe9aec0c3d93
Dmytro Siasko <dima.siasko@gmail.com>
2023-11-18 15:55:00 +0200

    Added sports data data file, added Sport model, added content main xml file, implemented activity main

---
+++ app/src/main/java/com/example/sports_app/model/Sport.kt
@@ -0,0 +1,12 @@
+package com.example.sports_app.model
+
+import com.example.sports_app.R
+
+data class Sport(
+    val id: Int,
+    val titleResourceId: Int,
+    val subTitleResourceId: Int,
+    val imageResourceId: Int,
+    val sportsImageBanner: Int,
+    val newsDetails: Int = R.string.sports_news_detail_text
+)
